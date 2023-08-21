b9fd881ce7fe457d1330c84c25a45cdc18a16b05
Jesse Haka <haka.jesse@gmail.com>
2023-08-21 10:02:23 +0300

    fetch id token from github

--- src/index.ts
+++ src/index.ts
@@ -17,49 +17,63 @@ import versionInfo from './version';
 
 let failCi;
 
-try {
-  const {execArgs, options, failCi, os, uploaderVersion, verbose} = buildExec();
-  const platform = getPlatform(os);
+/**
+ * Main function of the codecov-action
+ */
+async function run(): Promise<void> {
+  try {
+    const opts = await buildExec();
+    const platform = getPlatform(opts.os);
 
-  const filename = path.join( __dirname, getUploaderName(platform));
-  https.get(getBaseUrl(platform, uploaderVersion), (res) => {
+    const filename = path.join( __dirname, getUploaderName(platform));
+    https.get(getBaseUrl(platform, opts.uploaderVersion), (res) => {
     // Image will be stored at this path
-    const filePath = fs.createWriteStream(filename);
-    res.pipe(filePath);
-    filePath
-        .on('error', (err) => {
-          setFailure(
-              `Codecov: Failed to write uploader binary: ${err.message}`,
-              true,
-          );
-        }).on('finish', async () => {
-          filePath.close();
+      const filePath = fs.createWriteStream(filename);
+      res.pipe(filePath);
+      filePath
+          .on('error', (err) => {
+            setFailure(
+                `Codecov: Failed to write uploader binary: ${err.message}`,
+                true,
+            );
+          }).on('finish', async () => {
+            filePath.close();
 
-          await verify(filename, platform, uploaderVersion, verbose, failCi);
-          await versionInfo(platform, uploaderVersion);
-          await fs.chmodSync(filename, '777');
+            await verify(
+                filename,
+                platform,
+                opts.uploaderVersion,
+                opts.verbose,
+                failCi);
+            await versionInfo(platform, opts.uploaderVersion);
+            await fs.chmodSync(filename, '777');
 
-          const unlink = () => {
-            fs.unlink(filename, (err) => {
-              if (err) {
-                setFailure(
-                    `Codecov: Could not unlink uploader: ${err.message}`,
-                    failCi,
-                );
-              }
-            });
-          };
-          await exec.exec(filename, execArgs, options)
-              .catch((err) => {
-                setFailure(
-                    `Codecov: Failed to properly upload: ${err.message}`,
-                    failCi,
-                );
-              }).then(() => {
-                unlink();
+            const unlink = () => {
+              fs.unlink(filename, (err) => {
+                if (err) {
+                  setFailure(
+                      `Codecov: Could not unlink uploader: ${err.message}`,
+                      failCi,
+                  );
+                }
               });
-        });
-  });
-} catch (err) {
-  setFailure(`Codecov: Encountered an unexpected error ${err.message}`, failCi);
+            };
+            await exec.exec(filename, opts.execArgs, opts.options)
+                .catch((err) => {
+                  setFailure(
+                      `Codecov: Failed to properly upload: ${err.message}`,
+                      failCi,
+                  );
+                }).then(() => {
+                  unlink();
+                });
+          });
+    });
+  } catch (err) {
+    setFailure(
+        `Codecov: Encountered an unexpected error ${err.message}`,
+        failCi);
+  }
 }
+
+run();
